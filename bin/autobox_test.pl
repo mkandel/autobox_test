5927762787cad370afc9dd7c88a32666d5017e07
mkandel <marc.kandel.junk@gmail.com>
2013-03-13 14:58:41 -0700

    Initial commit, populating with dummy/template files

---
+++ bin/autobox_test.pl
@@ -0,0 +1,33 @@
+#!/opt/local/bin/perl -w
+# $Id:$
+# $HeadURL:$
+use strict;
+use warnings;
+
+use Carp;
+use Getopt::Long;
+Getopt::Long::Configure qw/bundling no_ignore_case/;
+use Data::Dumper;
+# Some Data::Dumper settings:
+local $Data::Dumper::Useqq  = 1;
+local $Data::Dumper::Indent = 3;
+use Pod::Usage;
+
+local $| = 1;
+
+my $mydebug = 0;
+my $dryrun  = 0;
+
+GetOptions(
+    "help|h"         => sub { pod2usage( 1 ); },
+    "debug|d"        => \$mydebug,
+    "dryrun|n"       => sub { $dryrun = 1; $mydebug = 1; },
+);
+
+my $prog = $0;
+$prog =~ s/^.*\///;
+
+## Code goes here
+
+__END__
+
